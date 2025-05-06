2bbf34252ed3d64506ca5751ffe7bb4298d83a4d
takemetilde <hongiaher@gmail.com>
2025-05-06 14:51:26 -0400

    Update GetEmailTokenHack.ts

--- tests/GetEmailTokenHack.ts
+++ tests/GetEmailTokenHack.ts
@@ -6,11 +6,11 @@ class GetEmailTokenHack {
         const page1Promise = page.waitForEvent('popup');
         await page.getByRole('button', {name: 'Sign in'}).click();
         const page1 = await page1Promise;
-        await page1.getByRole('textbox', {name: 'Enter your email, phone, or'}).fill('honeywell.autotest@outlook.com');
+        await page1.getByRole('textbox', {name: 'Enter your email, phone, or'}).fill('****.autotest@outlook.com');
         await page1.getByRole('button', {name: 'Next'}).click();
         if (await page1.locator('//input[@type="password"]').isVisible({timeout: 10000})) {
             await new Promise(resolve => setTimeout(resolve, 5000));
-            await page1.locator('//input[@type="password"]').fill('Setup123!');
+            await page1.locator('//input[@type="password"]').fill('******');
             if (await page1.getByRole('button', {name: 'Sign in'}).isVisible())
                 await page1.getByRole('button', {name: 'Sign in'}).click();
             else
